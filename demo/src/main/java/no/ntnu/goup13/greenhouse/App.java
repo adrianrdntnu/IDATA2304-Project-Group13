9bc1a8e81c9e871bf9de4707c417abc00d7c4b71
shv-gitman <98781052+shv-gitman@users.noreply.github.com>
2022-10-13 12:42:32 +0200

    added maven project

---
+++ demo/src/main/java/no/ntnu/goup13/greenhouse/App.java
@@ -0,0 +1,13 @@
+package no.ntnu.goup13.greenhouse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
