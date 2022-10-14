5b8af9e65b438de9550a4f6abb786b1c65073e39
shv-gitman <98781052+shv-gitman@users.noreply.github.com>
2022-10-14 12:58:29 +0200

    Cleaned up formatting

--- demo/src/main/java/no/ntnu/goup13/greenhouse/App.java
+++ demo/src/main/java/no/ntnu/goup13/greenhouse/App.java
@@ -4,10 +4,8 @@ package no.ntnu.goup13.greenhouse;
  * Hello world!
  *
  */
-public class App 
-{
-	public static void main( String[] args )
-	{
-		System.out.println( "Hello World!" );
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
 	}
 }
