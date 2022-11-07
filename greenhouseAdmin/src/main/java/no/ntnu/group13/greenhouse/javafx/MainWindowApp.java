e63aab93a089922c63e3568fea355ef7dcddc3e6
shv-gitman <98781052+shv-gitman@users.noreply.github.com>
2022-11-07 23:34:25 +0100

    renamed from demo to greenhouseAdmin

---
+++ greenhouseAdmin/src/main/java/no/ntnu/group13/greenhouse/javafx/MainWindowApp.java
@@ -0,0 +1,13 @@
+package no.ntnu.group13.greenhouse.javafx;
+
+public class MainWindowApp {
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+
+  }
+}
